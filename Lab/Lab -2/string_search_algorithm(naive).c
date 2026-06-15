515e5a20041798902f8685ba137126b7b75278b7
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-06-15 09:36:39 +0600

    Create string_search_algorithm(naive).c

---
+++ Lab/Lab -2/string_search_algorithm(naive).c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+
+void stringSearch(char text[], char pattern[]){
+    int n = strlen(text);
+    int m = strlen(pattern);
+
+    for(int i=0; i<=n-m; i++){
+
+        int j;
+        for(j=0; j<m; j++){
+           if(text[i+j] != pattern[j]) break;
+        }
+
+        if(j==m) printf("Pattern found at %d\n", i);
+    }
+}
+
+int main()
+{
+    char text[] = "aabaacaadaabaaba";
+    //gets(text);
+
+    char pattern[] = "aab";
+    //gets(pattern);
+
+    stringSearch(text, pattern);
+
+    return 0;
+}
+
